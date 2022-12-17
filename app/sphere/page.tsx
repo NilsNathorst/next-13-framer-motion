79a0f6280f9d361fb802c110eb18fa81166104c5
NilsNathorst <nilsnathorstwindahl@gmail.com>
2022-12-17 13:48:22 +0100

    add spring

--- app/sphere/page.tsx
+++ app/sphere/page.tsx
@@ -1,28 +1,29 @@
 "use client";
 import React, { useRef, useState } from "react";
 import { useFrame } from "@react-three/fiber";
+import { useSpring, animated, config } from "@react-spring/three";
 
 function Ball(props: any) {
-  // This reference will give us direct access to the mesh
   const mesh = useRef();
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
+
   const [active, setActive] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (mesh.current.rotation.x += delta));
-  // Return view, these are regular three.js elements expressed in JSX
+  const { scale } = useSpring({
+    scale: active ? 1.5 : 1,
+    config: config.wobbly,
+  });
+
+  useFrame((_, delta) => (mesh.current.rotation.x += delta));
+
   return (
-    <mesh
+    <animated.mesh
       {...props}
       ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
+      scale={scale}
+      onClick={() => setActive(!active)}
     >
       <sphereGeometry args={[1, 32, 16]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
+      <meshStandardMaterial color={"hotpink"} />
+    </animated.mesh>
   );
 }
 
